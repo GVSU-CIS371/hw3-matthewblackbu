2437fe658851d490450f57d96facc4dedd0a68b8
Matthew Blackburn <79994779+matthewblackbu@users.noreply.github.com>
2025-04-11 00:21:09 -0400

    .
    .

--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -6,15 +6,15 @@ import {
   BeverageType,
 } from "../types/beverage";
 import tempretures from "../data/tempretures.json";
-import db from "../firebase.ts";
+import db from "../firebase";
 import {
   collection,
   getDocs,
-  setDoc,
-  doc,
-  QuerySnapshot,
-  QueryDocumentSnapshot,
-  onSnapshot,
+  //setDoc,
+  //doc,
+  //QuerySnapshot,
+  //QueryDocumentSnapshot,
+  //onSnapshot,
 } from "firebase/firestore";
 
 export const useBeverageStore = defineStore("BeverageStore", {
@@ -33,9 +33,37 @@ export const useBeverageStore = defineStore("BeverageStore", {
   }),
 
   actions: {
-    init() {},
-    makeBeverage() {},
+    async init() {
+    try {
+      const  basesSnapshot = await getDocs(collection(db, 'bases'));
+      const creamersSnapshot = await getDocs(collection(db, 'creamers'));
+      const syrupsSnapshot = await getDocs(collection(db, 'syrups'));
 
+      this.bases = basesSnapshot.docs.map(
+        (doc): BaseBeverageType => ({ id: doc.id, ...(doc.data() as Omit<BaseBeverageType, 'id'>) })
+      );
+      
+      this.creamers = creamersSnapshot.docs.map(
+        (doc): CreamerType => ({ id: doc.id, ...(doc.data() as Omit<CreamerType, 'id'>) })
+      );
+      
+      this.syrups = syrupsSnapshot.docs.map(
+        (doc): SyrupType => ({ id: doc.id, ...(doc.data() as Omit<SyrupType, 'id'>) })
+      );
+      
+      this.currentBase = this.bases[0] || null;
+      this.currentCreamer = this.creamers[0] || null;
+      this.currentSyrup = this.syrups[0] || null;
+
+      } catch (error) {
+      console.error('Error initializing beverage store:', error);
+      }
+    },
+    makeBeverage() {},
+      
     showBeverage() {},
+    resetStore() {
+      this.$reset();
+    }
   },
 });
